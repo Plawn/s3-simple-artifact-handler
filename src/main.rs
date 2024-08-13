bf16b628e0f097873f41b2880e397ac5b35f859f
Plawn <plawn.yay@gmail.com>
2024-08-13 10:20:28 +0200

    with external config

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::Write;
+use std::io::{Error, Read, Write};
 use std::path::PathBuf;
 
 use awsregion::Region;
@@ -10,15 +10,25 @@ use log::debug;
 use s3::creds::Credentials;
 use s3::{Bucket, BucketConfiguration};
 use std::fs::{remove_file, File};
-use tar;
 use uuid::Uuid;
 
 type GenericErr = Box<dyn std::error::Error + Send + Sync>;
 
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct S3Conf {
+    endpoint: String,
+    access_key: String,
+    pass_key: String,
+}
+
 #[derive(Parser)]
 enum Cli {
     Upload {
-        #[arg(short, long)]
+        #[arg(long)]
+        config_file: PathBuf,
+        #[arg(long)]
         bucket: String,
         #[arg(long)]
         object: Option<String>,
@@ -28,8 +38,12 @@ enum Cli {
         files: Vec<PathBuf>,
     },
     Download {
+        #[arg(long)]
+        config_file: PathBuf,
         /// Bucket to upload the file to (will be created if it doesn't exist)
+        #[arg(long)]
         bucket: String,
+        #[arg(long)]
         object: String,
     },
 }
@@ -52,17 +66,22 @@ fn upload_file(bucket: Bucket, filename: &str, object_path: &str) -> Result<(), 
     debug!("uploading: {}", &filename);
     let bucket = ensure_bucket(bucket)?;
     // debug!("bucket is ok");
-    let mut async_output_file = File::open(filename).expect("Unable to create file");
+    let mut upload_file = File::open(filename).expect("Unable to create file");
     // let file_size = file.metadata()?.len();
-    let status_code = bucket.put_object_stream(&mut async_output_file, object_path)?;
-
+    let status_code = bucket.put_object_stream(&mut upload_file, object_path)?;
+    if status_code > 399 {
+        return Err(Box::new(Error::new(
+            std::io::ErrorKind::Other,
+            format!("Failed to upload: error -> {}", status_code),
+        )));
+    }
     Ok(())
 }
 fn recurse_files(path: impl AsRef<str>) -> Result<Vec<PathBuf>, PatternError> {
     let k = glob(path.as_ref()).map(|res| res.into_iter().map(|e| e.unwrap()).collect::<Vec<_>>());
     k
 }
-fn prepare_tar(paths: &Vec<PathBuf>) -> Result<String, GenericErr> {
+fn prepare_tar(paths: &[PathBuf]) -> Result<String, GenericErr> {
     let tar_name = "export.tar.gz";
 
     let tar_file = File::create(tar_name)?;
@@ -72,7 +91,7 @@ fn prepare_tar(paths: &Vec<PathBuf>) -> Result<String, GenericErr> {
     let mut tar_builder = tar::Builder::new(enc);
     let files = paths
         .iter()
-        .map(|e| recurse_files(&e.to_str().unwrap()))
+        .map(|e| recurse_files(e.to_str().unwrap()))
         .flat_map(|e| e.unwrap());
     for name in files {
         tar_builder.append_path(&name)?;
@@ -88,18 +107,22 @@ fn upload_artifacts(
     dirs: &Vec<PathBuf>,
     object: Option<String>,
 ) -> Result<String, GenericErr> {
-    let filename = prepare_tar(&dirs)?;
-    // TODO: generate random name
+    let filename = prepare_tar(dirs)?;
 
     let object = object.unwrap_or_else(|| {
         let id = Uuid::new_v4();
         id.to_string()
     });
     let name = &bucket.name.clone();
-    upload_file(bucket, &filename, &object)?;
+    let upload_result = upload_file(bucket, &filename, &object);
     remove_file(filename)?;
-    debug!("Uploaded files at {:?} to {}/{}", dirs, name, object);
-    Ok(object.to_string())
+    return match upload_result {
+        Ok(_) => {
+            debug!("Uploaded files at {:?} to {}/{}", dirs, name, object);
+            Ok(object.to_string())
+        },
+        Err(err) => Err(err),
+    }
 }
 
 fn download_artifacts(
@@ -117,44 +140,48 @@ fn download_artifacts(
     a.unpack(decode_location).map(|_| Ok(()))?
 }
 
+fn get_bucket(bucket_name: String, path: &PathBuf) -> Result<Bucket, GenericErr> {
+    let mut conf_file = File::open(path).expect("Unable to create file");
+    let mut buf = String::new();
+    conf_file.read_to_string(&mut buf)?;
+    let conf: S3Conf = toml::from_str(&buf)?;
+    let region = Region::Custom {
+        region: "us-east-1".to_owned(),
+        endpoint: conf.endpoint,
+    };
+    let credentials = Credentials::new(Some(&conf.access_key), Some(&conf.pass_key), None, None, None)?;
+    let bucket = Bucket::new(&bucket_name, region, credentials)?.with_path_style();
+    Ok(bucket)
+}
+
 fn main() -> Result<(), GenericErr> {
     let args = Cli::parse();
 
-    // TODO: take provider from  stdin
-
-    let access_key = "Q3AM3UQ867SPQQA43P2F";
-    let password_key = "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG";
-    let endpoint = "https://play.min.io";
-
-    let region = Region::Custom {
-        region: "us-east-1".to_owned(),
-        endpoint: endpoint.to_owned(),
-    };
-    let credentials = Credentials::new(Some(access_key), Some(password_key), None, None, None)?;
-
-    let result = match args {
+    match args {
         Cli::Upload {
             bucket: bucket_name,
             files,
             object,
+            config_file,
         } => {
-            let bucket = Bucket::new(&bucket_name, region, credentials)?.with_path_style();
+            let bucket = get_bucket(bucket_name, &config_file)?;
             let archive_name = upload_artifacts(bucket, &files, object)?;
-            // write archive name to file
-            debug!("{}", &archive_name);
+            println!("{}", &archive_name);
         }
         Cli::Download {
             bucket: bucket_name,
             object,
+            config_file,
         } => {
             let download_path = "local_download.tar.gz";
             let decode_location = ".";
-            let bucket = Bucket::new(&bucket_name, region, credentials)?.with_path_style();
+            // let bucket = Bucket::new(&bucket_name, region, credentials)?.with_path_style();
+            let bucket = get_bucket(bucket_name, &config_file)?;
             debug!("downloading :{}", &object);
             download_artifacts(&bucket, &object, download_path, decode_location)?;
             remove_file(download_path).unwrap();
         }
     };
 
-    Ok(result)
+    Ok(())
 }
